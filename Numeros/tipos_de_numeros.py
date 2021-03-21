72ce294e8331f72daab8d709a90aa72d5269c64b
AlexisNarvaez <alexisnarvaez97@hotmail.com>
2021-03-20 18:05:12 -0600

    numeros :monocle_face: : tipos_de_numeros

--- Numeros/tipos_de_numeros.py
+++ Numeros/tipos_de_numeros.py
@@ -1,1 +1,41 @@
-print('Hola Mundo')
+''' 
+    Tipos de números
+    
+    int: número entero
+    float: número en coma flotante
+'''
+
+
+''' 
+Para saber el tipo de dato de un número podemos utilizar la función type()
+'''
+
+
+print(type(5))
+
+print(type(5.0))
+
+print(type(3.0))
+
+print(type(3.))
+
+
+''' 
+    Convertir entero a flotante
+'''
+
+print(type(int(7.0)))
+
+print(type(int(7.)))
+
+print(type(float(3)))
+
+
+''' 
+    Convertir un flotante a entero
+    (CUIDADO)
+'''
+
+print(int(3.5)) # 3
+print(int(8.9)) # 8
+print(int(-8.9)) # -8
